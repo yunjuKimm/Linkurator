0a8a748c9500c26525a9534bfa521fca049f95d5
SDHSeoulTech <dongseoultech@gmail.com>
2025-02-28 17:31:45 +0900

    feat: LinkEntity

---
+++ backend/project2/src/main/java/com/team8/project2/domain/link/entity/Link.java
@@ -0,0 +1,37 @@
+package com.team8.project2.domain.link.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Link")
+public class Link {
+
+    @Id
+    @Column(name = "linkId", nullable = false)
+    private String id;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "click", nullable = false)
+    private int click;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "createdAt", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "thumbnail", nullable = false)
+    private String thumbnail;
+}
