3318c39154f5a6c6aa9983aae2f242ec22a34d96
kozae00 <rhwogus777@gmail.com>
2025-02-28 17:11:28 +0900

    feat: controller

--- backend/project2/src/main/java/com/team8/project2/domain/comment/controller/ApiV1CommentController.java
+++ backend/project2/src/main/java/com/team8/project2/domain/comment/controller/ApiV1CommentController.java
@@ -1,4 +1,33 @@
 package com.team8.project2.domain.comment.controller;
 
+import com.team8.project2.domain.comment.dto.CommentDto;
+import com.team8.project2.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/comments")
 public class ApiV1CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CommentDto> createComment(@RequestBody CommentDto commentDto) {
+        return ResponseEntity.ok(commentService.createComment(commentDto));
+    }
+
+    @GetMapping("/{curationId}")
+    public ResponseEntity<List<CommentDto>> getCommentsByCurationId(@PathVariable Long curationId) {
+        return ResponseEntity.ok(commentService.getCommentsByCurationId(curationId));
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
+        commentService.deleteComment(commentId);
+        return ResponseEntity.noContent().build();
+    }
 }
