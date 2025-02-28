a760797644d0e6a4ab27b1d94385bf03457d6bcc
SDHSeoulTech <dongseoultech@gmail.com>
2025-02-28 17:34:11 +0900

    feat: CurationEntity

---
+++ backend/project2/src/main/java/com/team8/project2/domain/curation/entity/Curation.java
@@ -0,0 +1,36 @@
+package com.team8.project2.domain.curation.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Curation")
+public class Curation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "curationId")
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "createdAt", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "modifiedAt")
+    private LocalDateTime modifiedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId", nullable = false)
+    private Member member;
+
+}
