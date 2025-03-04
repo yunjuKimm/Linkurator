ed8ba01f80d591e7addc9a3f08c1368ea568147c
kozae00 <rhwogus777@gmail.com>
2025-03-04 10:43:13 +0900

    feat: repository

--- backend/project2/src/main/java/com/team8/project2/domain/playlist/repository/PlaylistRepository.java
+++ backend/project2/src/main/java/com/team8/project2/domain/playlist/repository/PlaylistRepository.java
@@ -1,4 +1,9 @@
 package com.team8.project2.domain.playlist.repository;
 
-public interface PlaylistRepository {
+import com.team8.project2.domain.playlist.entity.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 }
--- backend/project2/src/main/java/com/team8/project2/domain/playlist/service/PlaylistService.java
+++ backend/project2/src/main/java/com/team8/project2/domain/playlist/service/PlaylistService.java
@@ -1,4 +1,13 @@
 package com.team8.project2.domain.playlist.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class PlaylistService {
+
+
 }
