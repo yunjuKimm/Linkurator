9637246906341737e5f22b10ec9be084141c9df4
kozae00 <rhwogus777@gmail.com>
2025-03-04 10:41:12 +0900

    feat: Dto

---
+++ backend/project2/src/main/java/com/team8/project2/domain/playlist/dto/PlaylistItemDto.java
@@ -0,0 +1,21 @@
+package com.team8.project2.domain.playlist.dto;
+
+import com.team8.project2.domain.playlist.entity.PlaylistItem;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PlaylistItemDto {
+    private Long id;
+    private Long itemId;
+    private String itemType;
+
+    public static PlaylistItemDto fromEntity(PlaylistItem playlistItem) {
+        return PlaylistItemDto.builder()
+                .id(playlistItem.getId())
+                .itemId(playlistItem.getItemId())
+                .itemType(playlistItem.getItemType())
+                .build();
+    }
+}
