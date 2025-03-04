5b5ef462c2aff9661d98f67840989248ad046ea6
kozae00 <rhwogus777@gmail.com>
2025-03-04 09:26:01 +0900

    refactor: commentservice modified

--- backend/project2/src/main/java/com/team8/project2/domain/comment/service/CommentService.java
+++ backend/project2/src/main/java/com/team8/project2/domain/comment/service/CommentService.java
@@ -3,6 +3,7 @@ package com.team8.project2.domain.comment.service;
 import com.team8.project2.domain.comment.dto.CommentDto;
 import com.team8.project2.domain.comment.entity.Comment;
 import com.team8.project2.domain.comment.repository.CommentRepository;
+import com.team8.project2.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,25 +19,21 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     public CommentDto createComment(CommentDto commentDto) {
-        Comment comment = Comment.builder()
-                .memberId(commentDto.getMemberId())
-                .curationId(commentDto.getCurationId())
-                .content(commentDto.getContent())
-                .build();
-
+        Comment comment = commentDto.toEntity();
         Comment savedComment = commentRepository.save(comment);
         return CommentDto.fromEntity(savedComment);
     }
 
     public List<CommentDto> getCommentsByCurationId(Long curationId) {
-        return commentRepository.findByCurationId(curationId)
-                .stream()
+        List<Comment> comments = commentRepository.findByCurationId(curationId);
+        return comments.stream()
                 .map(CommentDto::fromEntity)
                 .collect(Collectors.toList());
     }
 
-
     public void deleteComment(Long commentId) {
-        commentRepository.deleteById(commentId);
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ServiceException("COMMENT_NOT_FOUND", "해당 댓글을 찾을 수 없습니다."));
+        commentRepository.delete(comment);
     }
 }
