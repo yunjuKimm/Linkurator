d75f8258743dd1697ecb34975d31b9fe9249c9d8
kozae00 <rhwogus777@gmail.com>
2025-03-04 09:22:48 +0900

    refactor: ApiV1CommentController modified

--- backend/project2/src/main/java/com/team8/project2/domain/comment/controller/ApiV1CommentController.java
+++ backend/project2/src/main/java/com/team8/project2/domain/comment/controller/ApiV1CommentController.java
@@ -2,6 +2,7 @@ package com.team8.project2.domain.comment.controller;
 
 import com.team8.project2.domain.comment.dto.CommentDto;
 import com.team8.project2.domain.comment.service.CommentService;
+import com.team8.project2.global.dto.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,18 +17,20 @@ public class ApiV1CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<CommentDto> createComment(@RequestBody CommentDto commentDto) {
-        return ResponseEntity.ok(commentService.createComment(commentDto));
+    public ResponseEntity<RsData<CommentDto>> createComment(@RequestBody CommentDto commentDto) {
+        CommentDto createdComment = commentService.createComment(commentDto);
+        return ResponseEntity.ok(RsData.success(createdComment));
     }
 
     @GetMapping("/{curationId}")
-    public ResponseEntity<List<CommentDto>> getCommentsByCurationId(@PathVariable Long curationId) {
-        return ResponseEntity.ok(commentService.getCommentsByCurationId(curationId));
+    public ResponseEntity<RsData<List<CommentDto>>> getCommentsByCurationId(@PathVariable Long curationId) {
+        List<CommentDto> comments = commentService.getCommentsByCurationId(curationId);
+        return ResponseEntity.ok(RsData.success(comments));
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
+    public ResponseEntity<RsData<Empty>> deleteComment(@PathVariable Long commentId) {
         commentService.deleteComment(commentId);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(RsData.success(new Empty()));
     }
 }
