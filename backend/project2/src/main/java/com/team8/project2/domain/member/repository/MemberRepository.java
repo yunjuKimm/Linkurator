c75049683b65ebff3e204041eed1c9495005023c
taesu0608 <taesu0608@gmail.com>
2025-03-05 15:39:16 +0900

    feat : Member join 추가

--- backend/project2/src/main/java/com/team8/project2/domain/member/repository/MemberRepository.java
+++ backend/project2/src/main/java/com/team8/project2/domain/member/repository/MemberRepository.java
@@ -1,2 +1,11 @@
-package com.team8.project2.domain.member.repository;public interface MemberRepository {
+package com.team8.project2.domain.member.repository;
+
+
+import com.team8.project2.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByApiKey(String apiKey);
 }
--- backend/project2/src/main/java/com/team8/project2/domain/member/service/MemberService.java
+++ backend/project2/src/main/java/com/team8/project2/domain/member/service/MemberService.java
@@ -1,2 +1,36 @@
-package com.team8.project2.domain.member.service;public class MemberService {
+package com.team8.project2.domain.member.service;
+
+import com.team8.project2.domain.member.entity.Member;
+import com.team8.project2.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    public Member join(String userId, String username, String password, String apiKey, String role, String email) {
+        return join(userId, username, password, apiKey, role, email, null, null);
+    }
+
+    @Transactional
+    public Member join(String userId, String username, String password, String apiKey, String role, String imgUrl, String email, String description) {
+
+        //TODO: apikey 할당방식 지정
+        //TODO: RoleEnum 확인 이후 주입 로직 필요
+        Member member = Member.builder()
+                .userId(userId)
+                .username(username)
+                .apiKey(username)
+                .password(password)
+                .imgUrl(imgUrl)
+                .email(email)
+                .description(description).build();
+        return memberRepository.save(member);
+    }
+    public Optional<Member> findByApiKey(String apiKey) { return memberRepository.findByApiKey(apiKey);}
 }
