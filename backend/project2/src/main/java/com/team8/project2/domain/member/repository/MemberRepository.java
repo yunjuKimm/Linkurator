d1f2541364d671c52f1f9faf9892a6cb018f621e
socra167 <socra167@gmail.com>
2025-03-06 11:16:16 +0900

    chore: 샘플 데이터 초기화를 위해 `BaseInitData` 작성

--- backend/project2/src/main/java/com/team8/project2/domain/member/repository/MemberRepository.java
+++ backend/project2/src/main/java/com/team8/project2/domain/member/repository/MemberRepository.java
@@ -1,2 +1,8 @@
-package com.team8.project2.domain.member.repository;public interface MemberRepository {
+package com.team8.project2.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team8.project2.domain.member.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
