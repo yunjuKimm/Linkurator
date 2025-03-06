42aa596a10c76d24eaedef7dcdc759379b607023
socra167 <socra167@gmail.com>
2025-03-06 15:11:15 +0900

    chore: `BaseInitData`에서 샘플 데이터 생성 시 url, tag를 포함한 큐레이션을 추가하도록 변경

--- backend/project2/src/main/java/com/team8/project2/global/init/BaseInitData.java
+++ backend/project2/src/main/java/com/team8/project2/global/init/BaseInitData.java
@@ -1,11 +1,15 @@
 package com.team8.project2.global.init;
 
+import java.util.List;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.team8.project2.domain.curation.curation.entity.Curation;
 import com.team8.project2.domain.curation.curation.repository.CurationRepository;
+import com.team8.project2.domain.curation.curation.service.CurationService;
 import com.team8.project2.domain.member.entity.Member;
 import com.team8.project2.domain.member.entity.RoleEnum;
 import com.team8.project2.domain.member.repository.MemberRepository;
@@ -17,9 +21,12 @@ import lombok.RequiredArgsConstructor;
 public class BaseInitData {
 
 	private final MemberRepository memberRepository;
+	private final CurationRepository curationRepository;
+	private final CurationService curationService;
 
+	@Transactional
 	@Bean
-	public ApplicationRunner init(CurationRepository curationRepository) {
+	public ApplicationRunner init() {
 		return args -> {
 			if (memberRepository.count() == 0 && curationRepository.count() == 0) {
 				Member member = Member.builder()
@@ -33,12 +40,12 @@ public class BaseInitData {
 					.build();
 				memberRepository.save(member);
 
-				Curation curation = Curation.builder()
-					.title("test")
-					.content("test")
-					.member(member)
-					.build();
-				curationRepository.save(curation);
+				curationService.createCuration(
+					"curation test title",
+					"curation test content",
+					List.of("url1", "url2"),
+					List.of("tag1", "tag2")
+				);
 			}
 		};
 	}
