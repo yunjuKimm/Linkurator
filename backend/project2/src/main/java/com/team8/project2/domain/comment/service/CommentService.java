c47015057ce38ae6bd3c01691a07a2600b094a5b
socra167 <socra167@gmail.com>
2025-03-10 14:59:03 +0900

    fix: 댓글 수정, 삭제 인가 오류 수정

--- backend/project2/src/main/java/com/team8/project2/domain/comment/service/CommentService.java
+++ backend/project2/src/main/java/com/team8/project2/domain/comment/service/CommentService.java
@@ -71,9 +71,6 @@ public class CommentService {
 	public void deleteComment(Long commentId, String username) {
 		Comment comment = commentRepository.findById(commentId)
 			.orElseThrow(() -> new ServiceException("404-2", "해당 댓글을 찾을 수 없습니다."));
-		if (!comment.getAuthor().getUsername().equals(username)) {
-			throw new ServiceException("403-2", "댓글을 삭제할 권한이 없습니다.");
-		}
 		commentRepository.delete(comment);
 	}
 
@@ -82,9 +79,6 @@ public class CommentService {
 
 		Comment comment = commentRepository.findById(commentId)
 			.orElseThrow(() -> new ServiceException("404-2", "해당 댓글을 찾을 수 없습니다."));
-		if (!comment.getAuthor().getUsername().equals(username)) {
-			throw new ServiceException("403-2", "댓글을 수정할 권한이 없습니다.");
-		}
 		comment.updateContent(commentDto.getContent());
 		return CommentDto.fromEntity(comment);
 	}
@@ -98,7 +92,7 @@ public class CommentService {
 		Comment comment = commentRepository.findById(commentId)
 			.orElseThrow(() -> new ServiceException("404-2", "해당 댓글을 찾을 수 없습니다."));
 
-		return comment.getAuthor().getUsername().equals(userDetails.getUsername());
+		return comment.getAuthor().getMemberId().equals(userDetails.getUsername());
 	}
 
 	// 댓글 삭제 권한 체크 메서드
@@ -110,7 +104,7 @@ public class CommentService {
 		Comment comment = commentRepository.findById(commentId)
 			.orElseThrow(() -> new ServiceException("404-2", "해당 댓글을 찾을 수 없습니다."));
 
-		return comment.getAuthor().getUsername().equals(userDetails.getUsername());
+		return comment.getAuthor().getMemberId().equals(userDetails.getUsername());
 	}
 }
 
