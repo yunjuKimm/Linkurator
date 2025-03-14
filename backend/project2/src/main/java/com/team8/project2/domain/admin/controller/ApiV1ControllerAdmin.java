bc162e6faf71dd3391e2fec03c37beb79ad5694c
kozae00 <rhwogus777@gmail.com>
2025-03-14 10:53:06 +0900

    fix: import 수정

--- backend/project2/src/main/java/com/team8/project2/domain/admin/controller/ApiV1ControllerAdmin.java
+++ backend/project2/src/main/java/com/team8/project2/domain/admin/controller/ApiV1ControllerAdmin.java
@@ -4,7 +4,6 @@ import com.team8.project2.domain.admin.dto.StatsResDto;
 import com.team8.project2.domain.admin.service.AdminService;
 import com.team8.project2.global.dto.RsData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
--- backend/project2/src/main/java/com/team8/project2/domain/admin/repository/AdminRepository.java
+++
@@ -1,4 +0,0 @@
-package com.team8.project2.domain.admin.repository;
-
-public interface AdminRepository {
-}
