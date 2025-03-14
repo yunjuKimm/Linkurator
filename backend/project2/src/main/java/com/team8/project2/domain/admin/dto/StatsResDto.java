2717bc09c7d3e594b73aa9b4a8e6ab290a7c44be
kozae00 <rhwogus777@gmail.com>
2025-03-14 09:44:15 +0900

    feat: 트래픽 통계

--- backend/project2/src/main/java/com/team8/project2/domain/admin/dto/StatsResDto.java
+++ backend/project2/src/main/java/com/team8/project2/domain/admin/dto/StatsResDto.java
@@ -1,4 +1,13 @@
 package com.team8.project2.domain.admin.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class StatsResDto {
+    private long totalCurationViews;
+    private long totalCurationLikes;
+    private long totalPlaylistViews;
+    private long totalPlaylistLikes;
 }
